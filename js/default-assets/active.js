bb8a620ad67c5d5984549b8c4726ab58009629a1
sotschiwebsite <47680597+sotschiwebsite@users.noreply.github.com>
2019-10-16 23:48:34 +0200

    2

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -197,7 +197,7 @@
     // :: 16.0 Countdown Active Code
     // *********************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2019/11/31", function (event) {
+        $("#clock").countdown("2019/10/18", function (event) {
             $(this).html(event.strftime("<div>%D <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
         });
     }

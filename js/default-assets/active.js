bef975ebcc6eac84ef925eba9c915af4d0301d6b
Sotschi <lorikthaqi26@gmail.com>
2022-02-23 00:04:33 +0100

    2

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -197,7 +197,7 @@
     // :: 16.0 Countdown Active Code
     // *********************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2021/12/25", function (event) {
+        $("#clock").countdown("2022/02/25", function (event) {
             $(this).html(event.strftime("<div>%D <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
         });
     }

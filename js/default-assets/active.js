26095e5abeeb34b6238c1fc90e8359bb424a7224
sotschiwebsite <47680597+sotschiwebsite@users.noreply.github.com>
2022-06-13 10:23:19 +0200

    Update active.js

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -197,7 +197,7 @@
     // :: 16.0 Countdown Active Code
     // *********************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2022/5/14", function (event) {
+        $("#clock").countdown("2022/6/17", function (event) {
             $(this).html(event.strftime("<div>%D <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div>"));
         });
     }
